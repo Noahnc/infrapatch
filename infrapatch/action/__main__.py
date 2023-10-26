05587926e40c9f9d72b5c0aaea8fc163b48af0b0
Noahnc <nc.canadea@gmail.com>
2023-10-26 08:28:55 +0200

    test action

--- infrapatch/action/__main__.py
+++ infrapatch/action/__main__.py
@@ -23,7 +23,6 @@ from pygit2 import Repository, Remote
 def main(debug: bool, default_registry_domain: str, registry_secrets_string: str, source_branch_name: str, target_branch_name: str, github_token: str, report_only: bool,
          working_directory: str,
          do_not_push: bool):
-
     setup_logging(debug)
     log.debug(f"Running infrapatch with the following parameters: "
               f"default_registry_domain={default_registry_domain}, "
@@ -35,70 +34,23 @@ def main(debug: bool, default_registry_domain: str, registry_secrets_string: str
               f"working_directory={working_directory}"
               )
     credentials = {}
+    working_directory = Path(working_directory)
     if registry_secrets_string is not None:
         credentials = get_credentials_from_string(registry_secrets_string)
     main_handler = build_main_handler(default_registry_domain=default_registry_domain, credentials_dict=credentials)
-    # resources = main_handler.get_all_terraform_resources(Path(working_directory))
-    #
-    # if report_only:
-    #     main_handler.print_resource_table(resources)
-    #     log.info("Report only mode is enabled. No changes will be applied.")
-    #     return
-    #
-    # upgradable_resources = get_upgradable_resources(resources)
-    # if len(upgradable_resources) == 0:
-    #     log.info("No upgradable resources found.")
-    #     return
+    resources = main_handler.get_all_terraform_resources(working_directory)
 
-    repo = Repository(working_directory)
-    # if repo.is_dirty():
-    #     raise Exception("Repository is dirty. Please commit your changes before running infrapatch.")
-    # check if the source branch exists
+    if report_only:
+        main_handler.print_resource_table(resources)
+        log.info("Report only mode is enabled. No changes will be applied.")
+        return
 
-    # Replace these with your own values
-    remote_name = "origin"
-    source_branch_ref = f"{remote_name}/{source_branch_name}"
-    target_branch_ref = f"{remote_name}/{target_branch_name}"
-    source_branch = repo.lookup_branch(source_branch_ref, pygit2.GIT_BRANCH_REMOTE)
-    if source_branch is None:
-        raise Exception(f"Source branch '{source_branch_name}' does not exist in remote '{remote_name}'.")
+    upgradable_resources = get_upgradable_resources(resources)
+    if len(upgradable_resources) == 0:
+        log.info("No upgradable resources found.")
+        return
 
-    # Fetch the source branch
-    remote = repo.remotes[remote_name]
-    remote.fetch()
-
-    # Check if the target branch exists
-    target_branch_ref = f"refs/remotes/{remote_name}/{target_branch_name}"
-    target_branch = repo.lookup_branch(target_branch_ref, pygit2.GIT_BRANCH_REMOTE)
-
-    # Fetch the target branch if it exists
-    if target_branch is not None:
-        remote.fetch(target_branch_name)
-
-    target_branch = repo.lookup_branch(target_branch_ref)
-
-    source_branch = [branch for branch in repo.remotes if branch.name == source_branch_ref]
-    target_branch = [branch for branch in repo.remotes if branch.name == target_branch_ref]
-
-    if len(source_branch) == 0:
-        raise Exception(f"Source branch '{source_branch_ref}' does not exist.")
-    source_branch = source_branch[0]
-
-    source_branch.fetch()
-
-    if len(target_branch) == 0:
-        target_branch = repo.remotes.create(target_branch_name)
-    else:
-        target_branch = target_branch[0]
-        target_branch.fetch()
-
-    target_branch.checkout()
-
-def push(repo, branch, remote_name='origin'):
-    ref = f"refs/heads/{branch}:refs/heads/{branch}"
-    for remote in repo.remotes:
-        if remote.name == remote_name:
-            remote.push(ref)
+    main_handler.update_resources(upgradable_resources, confirm=False, project_root=working_directory)
 
 
 def get_credentials_from_string(credentials_string: str) -> dict:
