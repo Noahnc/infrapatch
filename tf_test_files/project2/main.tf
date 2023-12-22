5ea81cd1842f6ac03d394deb7ae8a73176cfa0c1
Noah Canadea <noah.canadea@cmiag.ch>
2023-12-22 15:08:15 +0000

    feat(tf_test_files): Add module that should be ignored by infrapatch.

--- tf_test_files/project2/main.tf
+++ tf_test_files/project2/main.tf
@@ -3,3 +3,10 @@ module "test_module" {
   source  = "hashicorp/consul/aws"
   version = "0.2.0"
 }
+
+# This resource should be ignored by infrapatch
+# infrapatch_options: ignore_resource=true
+module "test_module6" {
+  source  = "hashicorp/consul/aws"
+  version = "0.2.0"
+}
